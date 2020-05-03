7d746e7c3cdc50765f9ffd2fcbf600275b80ec9f
Dev Boldly <devboldly@gmail.com>
2020-05-03 12:47:23 -0500

    Update package

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './components/Bootstrap';
-export * from './components/BootswatchSelect';
-export * from './components/FontAwesome';
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./components/Bootstrap"));
-__export(require("./components/BootswatchSelect"));
-__export(require("./components/FontAwesome"));
